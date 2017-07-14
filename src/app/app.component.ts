ecbf06e51629adcb3878c39c83c68c9a0afbdb2c
Jenkins <ron.hollander@ictu.nl>
2017-07-14 14:08:22 +0200

    Modal with Angular-CLI / Webpack.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
+import {Modal} from "angular2-modal/plugins/bootstrap";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<button (click)="onClick()">Alert</button>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(public modal: Modal) {
+  }
+
+  onClick() {
+    this.modal.alert()
+      .size('lg')
+      .showClose(true)
+      .title('A simple Alert style modal window')
+      .body(`
+            <h4>Alert is a classic (title/body/footer) 1 button modal window that 
+            does not block.</h4>
+            <b>Configuration:</b>
+            <ul>
+                <li>Non blocking (click anywhere outside to dismiss)</li>
+                <li>Size large</li>
+                <li>Dismissed with default keyboard key (ESC)</li>
+                <li>Close with button click</li>
+                <li>HTML content</li>
+            </ul>`)
+      .open();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ModalModule, OverlayRenderer, DOMOverlayRenderer, Overlay } from 'angular2-modal';
+import { Modal, BootstrapModalModule } from 'angular2-modal/plugins/bootstrap';
+
+const MODAL_PROVIDERS = [
+  Modal,
+  Overlay,
+  { provide: OverlayRenderer, useClass: DOMOverlayRenderer }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ModalModule,
+    BootstrapModalModule
   ],
-  providers: [],
+  providers: [MODAL_PROVIDERS],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
