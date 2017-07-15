4d554812a44de4a6d7854f7a8952f282d0f6f1e1
HardikDG <hardikdg.sa@gmail.com>
2017-07-15 14:26:47 +0530

    Confirmation event handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,30 @@
 import {Component} from "@angular/core";
 import {Modal} from "angular2-modal/plugins/bootstrap";
 
-@Component({
-  selector: 'app-root',
-  template: `<button (click)="onClick()">Alert</button>`,
-  styleUrls: ['./app.component.css']
-})
+@Component({selector: 'app-root', template: `<button (click)="onClick()">Alert</button>`, styleUrls: ['./app.component.css']})
 export class AppComponent {
-  constructor(public modal: Modal) {
-  }
+  constructor(public modal : Modal) {}
 
   onClick() {
-    this.modal.alert()
-      .size('lg')
+    const dialog = this
+      .modal
+      .confirm()
+      .size('sm')
       .showClose(true)
       .title('A simple Alert style modal window')
+      .okBtn('Accept')
+      .cancelBtn('Reject')
       .body(`
             <h4>Alert is a classic (title/body/footer) 1 button modal window that 
-            does not block.</h4>
-            <b>Configuration:</b>
-            <ul>
-                <li>Non blocking (click anywhere outside to dismiss)</li>
-                <li>Size large</li>
-                <li>Dismissed with default keyboard key (ESC)</li>
-                <li>Close with button click</li>
-                <li>HTML content</li>
-            </ul>`)
+            does not block.</h4>`)
       .open();
+
+    dialog.then((resultPromise) => {
+      return resultPromise
+        .result
+        .then((result) => {
+          console.log("Result: " + result);
+        }, () => console.log('Rejected!'));
+    })
   }
 }
